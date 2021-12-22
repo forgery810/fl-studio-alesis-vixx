ac35752d51f583fb042337342d48faeb4a74224f
forgery810 <76767272+forgery810@users.noreply.github.com>
2021-12-21 18:26:25 -0800

    Add files via upload
    updating to 1.1.0

---
+++ data.py
@@ -0,0 +1,95 @@
+# version 1.10
+
+
+button = {
+
+	"mod_wheel": 1, 			# ok it's not technically a button but it needs to go somewhere 	
+
+    #Transport control switches				
+	"record": 114,
+	"loop": 115,
+	"pattern_down": 116,
+	"pattern_up": 117,
+	"stop": 118,
+	"play": 119,
+	
+
+	#First row of switches 
+	"undo": 48,							
+	"set_metronome": 49,
+	"escape": 50,
+	"up": 51,
+	"browser": 52,
+	"rotate_window": 53,				
+	"step_rec": 54,
+	"save": 55,
+	"quantize": 56,
+	"rand_gen": 57,
+	
+				
+	#Second row of switches 
+	"solo": 99,		
+	"enter": 65,
+	"left": 66,
+	"down": 67,
+	"right": 68,	
+	"offset_range": 69,
+	"open_channel_sampler": 70,
+	"step_parameter": 71,
+
+	#Third row of switches
+	"song_mode_toggle": 80,				
+	"overdub": 81,
+	"view_playlist": 82,
+	"view_piano_roll": 83,
+	"view_channel_rack": 84,
+	"view_mixer": 85,
+	"view_plugin_picker": 86,
+	"pad_mode_toggle": 87,
+	"link_chan": 88,
+
+
+
+}
+
+mode = {
+	
+	0: "Standard",
+	1: "Step Pattern",
+	2: "Pad per Channel",
+
+}
+
+parameters = {
+	
+	0: "Pitch",
+	1: "Velocity",
+	2: "Release",
+	3: "Fine Pitch",
+	4: "Panning",
+	5: "Mod x",
+	6: "Mod y",
+}
+
+
+knob = {
+	"knob_one": 20,
+	"knob_two": 21,
+	"knob_three": 22,
+	"knob_four": 23,
+	"knob_five": 24,
+	"knob_six": 25,
+	"knob_seven": 26,
+	"knob_eight": 27,
+	"knob_nine": 28,
+	"knob_ten":29,
+	"knob_eleven":30,
+	"knob_twelve":31,
+	"knob_thirteen": 32,
+	"knob_fourteen": 33,
+	"knob_fifteen": 34,
+	"knob_sixteen": 35,
+}
+
+
+						
