bf67c07b14c9fea9e58479b4b97c6e380a6cf786
forgery810 <76767272+forgery810@users.noreply.github.com>
2022-01-17 21:52:26 -0800

    Add files via upload

---
+++ device_AlesisVIxx_1_5_2.py
@@ -0,0 +1,58 @@
+# name=Alesis VIxx-1.50 dl from git
+# Author: ts-forgery
+# Version 1.5
+
+
+
+import device
+import channels
+from process import Process
+from notes import Pads
+
+
+
+
+	# Function called when script starts
+
+def OnInit():
+
+
+	if device.isAssigned():					# check if device assigned
+		print("Device assigned - ver 1.50")
+		print(device.getName())
+		print("Port Number:")
+		print(device.getPortNumber())
+	else:
+		print("Not assigned - ver 1.50")
+
+	pad = Pads()
+
+
+	# Function called on every midi message
+
+def  OnMidiMsg(event):
+	process = Process(event)
+
+
+	# Function called on every beat
+
+def OnUpdateBeatIndicator(event):
+	pass
+
+
+
+
+
+
+def num_gen():
+	rand_obj = _random.Random()
+	rand_obj.seed()
+	rand_int = rand_obj.getrandbits(16) 
+	return rand_int 
+
+
+def PlayChannel(note, vel):												
+	channels.selectOneChannel(note-60)
+	channels.midiNoteOn(note-60, 60, vel)
+
+			
